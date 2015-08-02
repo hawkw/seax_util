31b49d1798143b40696c8f782371ada70302f5a6
Hawk Weisman <hi@hawkweisman.me>
2015-08-02 17:25:26 -0400

    Start on AtLocation

--- src/compiler_tools/location.rs
+++ src/compiler_tools/location.rs
@@ -1,5 +1,7 @@
 use std::fmt;
+use std::ops;
 
+/// Represents a location within a source code file
 #[derive(Copy, Clone, PartialEq, Debug)]
 #[cfg_attr(feature = "unstable",
     unstable(feature = "location") )]
@@ -12,6 +14,25 @@ pub struct Location {
     pub line: isize
 }
 
+/// Annotates a value with a `Location`
+#[cfg_attr(feature = "unstable",
+    unstable(feature = "location") )]
+pub struct AtLocation<T> {
+    #[cfg_attr(feature = "unstable",
+        unstable(feature = "location") )]
+    pub location: Location,
+    #[cfg_attr(feature = "unstable",
+        unstable(feature = "location") )]
+    pub value: T
+}
+
+impl Location {
+    /// Returns the absolute position in the file.
+    #[cfg_attr(feature = "unstable",
+        unstable(feature = "location") )]
+    pub fn absolute(&self) -> isize { self.col + self.line }
+}
+
 impl fmt::Display for Location {
     #[cfg_attr(feature = "unstable",
         unstable(feature = "location") )]
@@ -20,9 +41,19 @@ impl fmt::Display for Location {
     }
 }
 
-impl Location {
-    /// Returns the absolute position in the file.
+impl<T> fmt::Display for AtLocation<T> where T: fmt::Display {
     #[cfg_attr(feature = "unstable",
         unstable(feature = "location") )]
-    pub fn absolute(&self) -> isize { self.col + self.line }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} at {}", self.value, self.location)
+    }
+}
+
+impl<T> ops::Deref for AtLocation<T> {
+    type Target = T;
+    #[cfg_attr(feature = "unstable",
+        unstable(feature = "location") )]
+    fn deref(&self) -> &T {
+        &self.value
+    }
 }
